1febe819106d3a05b77254347444432d1f9d354a
Sven Gonschorek <sven.gonschorek@bi-solutions-hamburg.com>
2025-10-30 15:51:40 +0100

    master: add basic order blocks and fair value gaps

--- src/components/charts/candlestick_chart.py
+++ src/components/charts/candlestick_chart.py
@@ -10,7 +10,7 @@ project_root = os.path.join(script_dir, "..", "..", "..")
 sys.path.append(os.path.abspath(project_root))
 
 from src.api.bybit.history_data import get_data
-from src.ai.analysis.parse_analysis import get_liquidity_zones
+from src.ai.analysis.parse_analysis import get_liquidity_zones, get_order_blocks, get_fair_value_gaps
 
 def candlestick_chart(symbol,height, width):
     interval = "5"
@@ -20,6 +20,7 @@ def candlestick_chart(symbol,height, width):
         mapping = {
             '1m': '1',
             '5m': '5',
+            '15m': '15',
             '1h': '60',
             '4h': '240',
             '1d': 'D'
@@ -32,9 +33,10 @@ def candlestick_chart(symbol,height, width):
         option_map = {
             0: "1m",
             1: "5m",
-            2: "1h",
-            3: "4h",
-            4: "1d"
+            2: "15m",
+            3: "1h",
+            4: "4h",
+            5: "1d"
         }
 
         timeframe_switch = st.segmented_control(
@@ -78,7 +80,7 @@ def candlestick_chart(symbol,height, width):
         chart.set(data_loader())
 
         # Add liquidity zones as markers
-        liquidity_zones = get_liquidity_zones(symbol, interval)
+        liquidity_zones = get_liquidity_zones(symbol, int(interval) if interval not in ['D'] else 1440)
 
         if liquidity_zones["nearest_swing_high"] != {}:
             chart.marker(
@@ -97,5 +99,54 @@ def candlestick_chart(symbol,height, width):
                 shape="arrowUp",
                 position="below"
             )
+        
+        # Add order blocks as boxes
+        order_blocks = get_order_blocks(symbol)
+
+        if order_blocks:
+            supply = order_blocks["nearest_supply"]
+            chart.box(
+                start_time=pd.to_datetime(supply["timestamp"]),
+                end_time=pd.Timestamp.now(),
+                start_value=supply["price_high"],
+                end_value=supply["price_low"],
+                fill_color="#FF00001F",
+                color="#FF000000"
+            )
+
+            demand = order_blocks["nearest_demand"]
+            chart.box(
+                start_time=pd.to_datetime(demand["timestamp"]),
+                end_time=pd.Timestamp.now(),
+                start_value=demand["price_low"],
+                end_value=demand["price_high"],
+                fill_color="#04520446",
+                color="#00FF0000"
+            )
+        
+        # Add fair value gaps as boxes
+        fair_value_gaps = get_fair_value_gaps(symbol)
+
+        for fvg in fair_value_gaps:
+            print(fvg)
+            if fvg["timeframe"] == option_map[timeframe_switch]:
+                if fvg["type"] == "bullish":
+                    chart.box(
+                        start_time=pd.to_datetime(fvg["timestamp"]),
+                        end_time=pd.to_datetime(fvg["timestamp"]) + pd.Timedelta(minutes=int(interval) if interval not in ['D'] else 1440),
+                        start_value=fvg["price_low"],
+                        end_value=fvg["price_high"],
+                        fill_color="#054D127C",
+                        color="#054D12FF"
+                    )
+                else:
+                    chart.box(
+                        start_time=pd.to_datetime(fvg["timestamp"]),
+                        end_time=pd.to_datetime(fvg["timestamp"]) + pd.Timedelta(minutes=int(interval) if interval not in ['D'] else 1440),
+                        start_value=fvg["price_low"],
+                        end_value=fvg["price_high"],
+                        fill_color="#FF22007D",
+                        color="#560000FF"
+                    )
 
         chart.load()
